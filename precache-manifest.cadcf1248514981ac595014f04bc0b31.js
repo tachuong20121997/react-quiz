87fbbba5f78b677f2cc1fb7b57beb4e12e23aa0b
Chuong Tran <39957959+tachuong20121997@users.noreply.github.com>
2020-10-25 21:34:03 +0700

    Updates

---
+++ precache-manifest.cadcf1248514981ac595014f04bc0b31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46753ef8db285a14faaac403789d5ff",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "fd8dc92daeecd332b9e8",
+    "url": "/react-quiz/static/css/main.38a3407c.chunk.css"
+  },
+  {
+    "revision": "222b9d06771f2c26e582",
+    "url": "/react-quiz/static/js/2.0f7fe704.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/react-quiz/static/js/2.0f7fe704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd8dc92daeecd332b9e8",
+    "url": "/react-quiz/static/js/main.9a2ecbbc.chunk.js"
+  },
+  {
+    "revision": "928f8d10e2d93c26f2dd",
+    "url": "/react-quiz/static/js/runtime-main.acabde0b.js"
+  },
+  {
+    "revision": "aa13d9e2570a04eb995675133d365f62",
+    "url": "/react-quiz/static/media/bg.aa13d9e2.jpg"
+  }
+]);
